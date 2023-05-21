f0e04ad3facf5560dc7352959f7be59ba8c83631
PLayL04 <playl04@yandex.ru>
2023-05-21 15:33:50 +0700

    Bindings n Doors
    Сделаны двери и взаимодействие

---
+++ Assets/Scripts/DoorBehaviour.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class DoorBehaviour : MonoBehaviour, IInteractable
+{
+    public Animator Animator;
+    public bool isOpen;
+
+    public void Start()
+    {
+        Animator = GetComponent<Animator>();
+    }
+
+    public void Interact()
+    {
+        Animator.SetBool("isOpenDoor1", isOpen);
+        isOpen = !isOpen;
+    }
+}
--- Assets/Scripts/InteractionController.cs
+++ Assets/Scripts/InteractionController.cs
@@ -3,8 +3,31 @@ using UnityEngine.InputSystem;
 
 public class InteractionController : MonoBehaviour
 {
-    public void OnInteractionTrigger(InputAction.CallbackContext context)
+    public Camera camera;
+    public float interactionDistance = 5f;
+
+    public void Start()
     {
-        Debug.Log($"Взаимодействие: состояние {context.phase}");
+        camera = Camera.main;
+    }
+
+    public void OnInteractionTrigger(InputAction.CallbackContext input)
+    {
+        Ray ray = camera.ViewportPointToRay(Vector3.one / 2f);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, interactionDistance) && input.phase == InputActionPhase.Started)
+        {
+            var interactable = hit.collider.GetComponent<IInteractable>();
+            if (interactable != null)
+            {
+                interactable.Interact();
+                Debug.Log("Взаимодействие");
+            }
+            else
+            {
+                Debug.Log("Не с чем взаимодейсвовать");
+            }
+        }
     }
 }
